73f366c6835d46bf152704da812138678a3c8fbb
luoqianshi <1309251728@qq.com>
2024-04-28 22:03:19 +0800

    1. 已经解决在QPixmap和QScrollArea中间加一层QWidget 出现了QScrollArea的滑条无法正常使用的问题。

--- scattered examples/learn_QScrollArea_with_inner_widget.py
+++ scattered examples/learn_QScrollArea_with_inner_widget.py
@@ -3,6 +3,7 @@
 我们在QPixmap和QScrollArea中间加一层QWidget
 出现了QScrollArea的滑条无法正常使用的问题！！！
 !!!!!
+已解决
 """
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QScrollArea, QMainWindow
@@ -24,6 +25,9 @@ class ScrollExample(QMainWindow):
         # 创建一个标签用于显示图片
         image_label = QLabel(inner_widget)
         image_label.setPixmap(pixmap)
+
+        # 加上这一条语句之后就可以让滑动条正常工作了！牛逼！
+        inner_widget.setMinimumSize(pixmap.size())
 
         # 创建一个滚动区域并设置内容
         scroll_area = QScrollArea()
